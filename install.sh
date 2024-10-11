ef6c5a79ef3c9bed7f4b9d85126a79a1664465a5
iHad168 <122293678+iHad168@users.noreply.github.com>
2024-10-11 09:49:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pkg install proot-distro
-wget -P ~/../usr/etc/proot-distro/ --no-cacahe https://raw.githubusercontent.com/TodoWorld/SteamDroidOS/main/SteamDroidOS.sh
+wget -P ~/../usr/etc/proot-distro/ https://raw.githubusercontent.com/TodoWorld/SteamDroidOS/main/SteamDroidOS.sh
 pd reset SteamDroidOS
